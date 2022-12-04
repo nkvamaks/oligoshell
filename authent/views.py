9a836dc66f32e13b44d3d6eb76ab40d4d946d77c
MVK <55558386+nkvamaks@users.noreply.github.com>
2022-12-04 23:17:11 +0100

    Add minor changes

--- authent/views.py
+++ authent/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.contrib.auth import authenticate, login
+# from django.http import HttpResponse
 
-from . import forms
-
+# from . import forms
+#
 # def user_login(request):
 #     return render(request, 'oligoshell/profile.html', {'user': request.user})
--- osh/settings.py
+++ osh/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kz5@h6idu5q*zolq*r2j!t8tcn6=pq7ilbk=#icrr#k+*^rkap'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'osh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'oligos',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
